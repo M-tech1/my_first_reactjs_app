8c6b0ac9ff2b3ca55017e1c7886d217fb6f906de
martins <akemartins13@gmail.com>
2023-01-16 15:05:23 -0800

    First react js app

---
+++ src/ChildComponent.js
@@ -0,0 +1,5 @@
+const ChildComponent = (props) => {  
+    return <p className="children">{props.text}</p>; 
+  };
+  
+  export default ChildComponent;
---
+++ src/ParentComponent.js
@@ -0,0 +1,12 @@
+import ParentComponent from './ParentComponent.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ParentComponent />
+    </div>
+  );
+}
+
+export default App;
