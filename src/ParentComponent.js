dd74a4063a7c3c4f466cc266a1ab20215f4a6760
martins <akemartins13@gmail.com>
2023-01-17 03:40:34 -0800

    phase two of learning react task

--- src/ParentComponent.js
+++ src/ParentComponent.js
@@ -1,12 +1,31 @@
-import ParentComponent from './ParentComponent.js';
-import './App.css';
+// import ParentComponent from './ParentComponent.js';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <ParentComponent />
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <ParentComponent />
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+
+
+import React, { Component } from 'react';
+import ChildComponent from './ChildComponent.js';
+
+export class ParentComponent extends Component {  
+    render() {
+      return (
+        <h1>
+          <span className="title">I'm the parent component.</span>
+          <ChildComponent text={"i'm the 1st child"} />
+          <ChildComponent text={"i'm the 2st child"} />
+          <ChildComponent text={"i'm the 3st child"} />
+        </h1>
+      );
+    }
+  }
+
+export default ParentComponent;
